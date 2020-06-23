3025a630321b84c434979e2a323f448f1b1eed1a
TirupalRavila <rao.tirupal@gmail.com>
2020-06-23 14:41:53 -0400

    Formatting and contact update

--- js/contact_me.js
+++ js/contact_me.js
@@ -1,11 +1,11 @@
-$(function() {
+$(function () {
 
   $("#contactForm input,#contactForm textarea").jqBootstrapValidation({
     preventSubmit: true,
-    submitError: function($form, event, errors) {
+    submitError: function ($form, event, errors) {
       // additional error messages or events
     },
-    submitSuccess: function($form, event) {
+    submitSuccess: function ($form, event) {
       event.preventDefault(); // prevent default submit behaviour
       // get values from FORM
       var name = $("input#name").val();
@@ -26,41 +26,41 @@ $(function() {
       // var email = $("input#email").val();
       // var phone = $("input#phone").val();
       // var message = $("textarea#message").val();
-      form.appendChild(document.getElementById("name").cloneNode( true ))
-      form.appendChild(document.getElementById("email").cloneNode( true ))
-      form.appendChild(document.getElementById("phone").cloneNode( true ))
-      form.appendChild(document.getElementById("inmessage").cloneNode( true ))
+      form.appendChild(document.getElementById("name").cloneNode(true))
+      form.appendChild(document.getElementById("email").cloneNode(true))
+      form.appendChild(document.getElementById("phone").cloneNode(true))
+      form.appendChild(document.getElementById("inmessage").cloneNode(true))
 
       console.log("This: ", this)
-      console.log("Form:",form)
-      emailjs.sendForm('contact_service', 'contact_form', form).then(function(response) {
+      console.log("Form:", form)
+      emailjs.sendForm('contact_service', 'contact_form', form).then(function (response) {
         console.log('SUCCESS!', response.status, response.text);
-         // Success message
-         $('#success').html("<div class='alert alert-success'>");
-         $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-           .append("</button>");
-         $('#success > .alert-success')
-           .append("<strong>Your message has been sent. </strong>");
-         $('#success > .alert-success')
-           .append('</div>');
-         //clear all fields
-         $('#contactForm').trigger("reset");
-       
-     }, function(error) {
+        // Success message
+        $('#success').html("<div class='alert alert-success'>");
+        $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+          .append("</button>");
+        $('#success > .alert-success')
+          .append("<strong>Your message has been sent. </strong>");
+        $('#success > .alert-success')
+          .append('</div>');
+        //clear all fields
+        $('#contactForm').trigger("reset");
+
+      }, function (error) {
         console.log('FAILED...', error);
-         // Fail message
-         $('#success').html("<div class='alert alert-danger'>");
-         $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-           .append("</button>");
-         $('#success > .alert-danger').append($("<strong>").text("Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!"));
-         $('#success > .alert-danger').append('</div>');
-         //clear all fields
-         $('#contactForm').trigger("reset");
-     }, function() {
-      setTimeout(function() {
-        $this.prop("disabled", false); // Re-enable submit button when AJAX call is complete
-      }, 1000);
-    });
+        // Fail message
+        $('#success').html("<div class='alert alert-danger'>");
+        $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+          .append("</button>");
+        $('#success > .alert-danger').append($("<strong>").text("Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!"));
+        $('#success > .alert-danger').append('</div>');
+        //clear all fields
+        $('#contactForm').trigger("reset");
+      }, function () {
+        setTimeout(function () {
+          $this.prop("disabled", false); // Re-enable submit button when AJAX call is complete
+        }, 1000);
+      });
       /*$.ajax({
         url: "././mail/contact_me.php",
         type: "POST",
@@ -100,18 +100,18 @@ $(function() {
         }
       });*/
     },
-    filter: function() {
+    filter: function () {
       return $(this).is(":visible");
     },
   });
 
-  $("a[data-toggle=\"tab\"]").click(function(e) {
+  $("a[data-toggle=\"tab\"]").click(function (e) {
     e.preventDefault();
     $(this).tab("show");
   });
 });
 
 /*When clicking on Full hide fail/success boxes */
-$('#name').focus(function() {
+$('#name').focus(function () {
   $('#success').html('');
 });
